0e910a3c410e32ce3cd7c5112b7ea34d9831e179
Betto-lab <jesuslv159@gmail.com>
2026-09-10 17:30:24 -0500

    feat: Creacion de la API y mensaje con mi nombre

---
+++ api/main.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+const MESSAGE = process.env.MESSAGE || "Hola, soy Jesús y esta es mi API";
+
+
+app.get("/", (req, res) => {
+
+    res.json({
+        mensaje: MESSAGE
+    });
+
+});
+
+
+app.listen(PORT, () => {
+
+    console.log(`API ejecutándose en el puerto ${PORT}`);
+
+});
